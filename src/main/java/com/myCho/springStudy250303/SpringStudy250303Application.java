3b0f9c04a05a2b4a934616a58f985a9d37b8910e
HYOINCHO <hyoincho9123@gmail.com>
2025-03-03 17:48:34 +0900

    *공통화면 추가

---
+++ src/main/java/com/myCho/springStudy250303/SpringStudy250303Application.java
@@ -0,0 +1,13 @@
+package com.myCho.springStudy250303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy250303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudy250303Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myCho/springStudy250303/SpringStudy250303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myCho.springStudy250303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy250303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
